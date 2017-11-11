20ea64575aeedaeaf7a2e6af4c209fa9a7f8b88a
bcuric <bcuric@foi.hr>
2017-11-11 12:43:29 +0100

    Add project files.

---
+++ FitTv/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FitTv.Startup))]
+namespace FitTv
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
